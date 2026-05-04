5b3cc46a95a316b3966be1c21b1f1243fbf03e2f
Lara Muiña <152939155+lara0m@users.noreply.github.com>
2026-05-04 10:40:48 -0300

    parametros de ruta

--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
+
+camara = [
+    {
+    "id": 1,
+    "prediccion": "Robo",
+    "modelo": "Alpha 7 III",
+    "confianza": 0.95
+    }
+]
 
 app.title =  "Mi primer API con FastAPI"
 
@@ -8,3 +18,20 @@ app.title =  "Mi primer API con FastAPI"
 #funcion encargada de devolver un mensaje al acceder a la ruta raíz del servidor
 def home():
     return "Hello world!!"
+
+@app.get("/camaras", tags = ["Home"])
+#funcion encargada de devolver un mensaje al acceder a la ruta raíz del servidor
+def get_camaras():
+    #puede devolver distintos tipos de datos como diccionarios:
+    #return {"Hello": "World!!"}
+    #return HTMLResponse("<h1>Hola mundo!!</h1>")
+    return camara
+
+@app.get("/camaras/{id}", tags = ["Home"])
+#funcion encargada de devolver un mensaje al acceder a la ruta raíz del servidor
+def get_camara(id: int):
+    #puede devolver distintos tipos de datos como diccionarios:
+    #return {"Hello": "World!!"}
+    #return HTMLResponse("<h1>Hola mundo!!</h1>")
+    return id
+

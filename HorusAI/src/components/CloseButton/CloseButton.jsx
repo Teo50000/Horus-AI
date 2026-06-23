31822acce014eff96a0bfbcc99496d3ac88b9420
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2026-06-23 12:09:50 -0300

    mejoras de hover y proporciones de botones de navegación y de cerrar panel

--- HorusAI/src/components/CloseButton/CloseButton.jsx
+++ HorusAI/src/components/CloseButton/CloseButton.jsx
@@ -1,9 +1,10 @@
 import "./CloseButton.css";
+import CloseIcon from "../../assets/CloseSimbol.svg"
 
 export default function CloseButton({ onClick }) {
   return (
     <button className="close-button" onClick={onClick} title="Cerrar panel">
-      ‹
+      <img src={CloseIcon} alt="Cerrar" className="close__icon " />
     </button>
   );
 }

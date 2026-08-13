ebf658ec46214c044d6624567696ec4bdaf206cf
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2026-08-13 15:52:36 -0300

    rompí toda la aplicación
    logré que se conecte la app al backend, y despues la rompí intentando hacer que el back se ejecute en un doble click

--- HorusAI/src-tauri/src/lib.rs
+++ HorusAI/src-tauri/src/lib.rs
@@ -3,10 +3,25 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
+use std::process::Command;
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let backend_path = app
+                .path()
+                .resource_dir()
+                .expect("No se encontró resource dir")
+                .join("binaries/backend.exe");
+
+            Command::new(backend_path)
+                .spawn()
+                .expect("No se pudo iniciar el backend");
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())

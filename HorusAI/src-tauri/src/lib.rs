e2e2bd66c63e89be99eb06982d0243be8c84c779
Teo2602 <167240738+Teo2602@users.noreply.github.com>
2026-08-18 15:58:42 -0300

    logre que levante la app pero hay errores de routers

--- HorusAI/src-tauri/src/lib.rs
+++ HorusAI/src-tauri/src/lib.rs
@@ -1,27 +1,28 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-use std::process::Command;
-use tauri::Manager;
+
+use tauri_plugin_shell::ShellExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .setup(|app| {
-            let backend_path = app
-                .path()
-                .resource_dir()
-                .expect("No se encontró resource dir")
-                .join("binaries/backend.exe");
+            let sidecar = app
+                .shell()
+                .sidecar("backend")
+                .expect("No se pudo encontrar el backend");
 
-            Command::new(backend_path)
+            sidecar
                 .spawn()
                 .expect("No se pudo iniciar el backend");
 
             Ok(())
         })
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
